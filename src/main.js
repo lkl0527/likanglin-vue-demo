dd259fc1dfd2c3303c34b710c25728987a25b368
likl <likl@primeton.com>
2018-12-10 16:12:52 +0800

    更新Qs

--- src/main.js
+++ src/main.js
@@ -7,11 +7,13 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import ApiPlugin from '@/plugin/apis';
 import AjaxPlugin from '@/plugin/AjaxPlugin';
+import Qs from 'Qs';
 
 Vue.config.productionTip = false;
 Vue.use(ElementUI);
 Vue.use(ApiPlugin);
 Vue.use(AjaxPlugin);
+Vue.user(Qs);
 
 /* eslint-disable no-new */
 new Vue({
