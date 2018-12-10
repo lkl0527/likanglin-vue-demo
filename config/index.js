581c31e0f1ef8f0eb5832e6fa9d63e11d47948a9
likl <likl@primeton.com>
2018-12-10 20:07:05 +0800

    更新代理地址

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/apis': {    //将www.exaple.com印射为/apis
-        target: 'http://127.0.0.1:12319/api',  // 接口域名
+        target: 'http://10.15.15.140:12319/api',  // 接口域名
         secure: false,  // 如果是https接口，需要配置这个参数
         changeOrigin: true,  //是否跨域
         pathRewrite: {
